5d9004ac902e009d2737538ae8f3bd2ced68b586
Jason L Perry <jason@suncoast.io>
2018-03-20 16:37:31 -0400

    Start implementing template

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,25 +2,24 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { node } from 'prop-types';
 
-import logo from './logo.svg';
-import styles from './App.scss';
-
 export default function App(props) {
   return (
-    <div className={styles.app}>
-      <header className={styles.header}>
-        <img src={logo} className={styles.logo} alt="logo" />
-        <h1 className={styles.title}>Welcome to React</h1>
-      </header>
-      <div>
-        <NavLink exact to="/">Home</NavLink>
-        {' | '}
-        <NavLink exact to="/login">Login</NavLink>
-        {' | '}
-        <NavLink exact to="/dapp">DApp Example</NavLink>
-      </div>
-      <div>
-        {props.children}
+    <div className="all-wrapper with-side-panel solid-bg-all">
+      <div className="layout-w">
+        <div className="menu-w">
+          <NavLink exact to="/">
+            Home
+          </NavLink>
+          {' | '}
+          <NavLink exact to="/login">
+            Login
+          </NavLink>
+          {' | '}
+          <NavLink exact to="/dapp">
+            DApp Example
+          </NavLink>
+        </div>
+        <div>{props.children}</div>
       </div>
     </div>
   );
