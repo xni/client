5b9a1209de3b4e4a0b002fded1d816f0724343e8
Matt Huggins <matt.huggins@gmail.com>
2018-07-04 18:10:13 -0500

    feat(nav): clean up navigation

--- src/renderer/root/components/App/Routes/Routes.js
+++ src/renderer/root/components/App/Routes/Routes.js
@@ -5,8 +5,6 @@ import { Register } from 'register';
 import { Login } from 'login';
 import { Logout } from 'logout';
 import { Browser } from 'browser';
-import { Exchange } from 'exchange';
-import { Favorites } from 'favorites';
 import { Account } from 'account';
 import { Settings } from 'settings';
 
@@ -24,8 +22,6 @@ export default function Routes() {
       <Route exact path="/login" component={withLoginLayout(Login)} />
       <Route exact path="/register" component={withLoginLayout(Register)} />
       <PrivateRoute exact path="/browser" component={withAuthLayout(Browser)} />
-      <PrivateRoute exact path="/favorites" component={withAuthLayout(Favorites)} />
-      <PrivateRoute exact path="/exchange" component={withAuthLayout(Exchange)} />
       <PrivateRoute exact path="/account" component={withAuthLayout(Account)} />
       <PrivateRoute exact path="/settings" component={withAuthLayout(Settings)} />
       <PrivateRoute exact path="/logout" component={withAuthLayout(Logout)} />
